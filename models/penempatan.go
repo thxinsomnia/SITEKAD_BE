932091c2e82b6641c0082307c17dbdffdb56ee17
thxinsomnia <kompenizeko@gmail.com>
2025-09-08 16:44:21 +0700

    Add Struct Penempatan for Auth

---
+++ models/penempatan.go
@@ -0,0 +1,22 @@
+package models
+
+import "time"
+
+type Penempatan struct {
+	Id                int64     `gorm:"primaryKey" json:"id"`
+	Nomor             string    `gorm:"type varchar(200)" json:"nomor"`
+	Tgl               time.Time `gorm:"type date" json:"tgl"`
+	Lokasi_kerja_id   int64     `gorm:"type int" json:"lokasi_kerja_id"`
+	Pkwt_id           int64     `gorm:"type int" json:"pkwt_id"`
+	Jabatan_id        int64     `gorm:"type int" json:"jabatan_id"`
+	Created_at       time.Time `gorm:"type timestamp" json:"created_at"`
+	Updated_at       time.Time `gorm:"type timestamp" json:"updated_at"`
+	Created_by       int64     `gorm:"type int" json:"created_by"`
+	Updated_by       int64     `gorm:"type int" json:"updated_by"`
+	Is_deleted       int8      `gorm:"type tinyint" json:"is_deleted"`
+	Cabang_id       int64     `gorm:"type int" json:"cabang_id"`
+	Status_penempatan string    `gorm:"type varchar(100)" json:"status_penempatan"`
+	Username         string    `gorm:"type varchar(255)" json:"username"`
+	Password         string    `gorm:"type varchar(255)" json:"password"`
+	Salt             string    `gorm:"type varchar(255)" json:"salt"`
+}
