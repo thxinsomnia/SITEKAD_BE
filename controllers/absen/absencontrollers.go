e78f49e58be5ecb8199b6d4221874adc91717bd9
thxinsomnia <kompenizeko@gmail.com>
2025-09-08 16:43:57 +0700

    Testing Get Data Absensi

--- controllers/absen/absencontrollers.go
+++ controllers/absen/absencontrollers.go
@@ -8,14 +8,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//fungsi ini untuk mendapatkan semua data user
-func GetAllUser(c *gin.Context) {
-    var users []models.User
+//Histori Absensi
+func GetAllAbsen(c *gin.Context) {
+    var absensi []models.Absensi
 
-    if err := models.DB.Find(&users).Error; err != nil {
+    if err := models.DB.Order("created_at desc").Find(&absensi).Limit(10).Error; err != nil {
         c.JSON(http.StatusInternalServerError, gin.H{"Message": err.Error()})
         return
     }
 
-    c.JSON(http.StatusOK, gin.H{"Users": users})
+    c.JSON(http.StatusOK, gin.H{"Absen": absensi})
 }
