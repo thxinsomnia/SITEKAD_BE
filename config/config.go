c4779dca3cd3bcfb41079e92f66ea78d3f2578f0
thxinsomnia <kompenizeko@gmail.com>
2025-09-10 15:07:42 +0700

    Fix config

--- config/config.go
+++ config/config.go
@@ -1,9 +1,29 @@
 package config
 
-import "github.com/golang-jwt/jwt/v4"
+import (
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
 
-var JWT_KEY = []byte("qwertyuiop1234567890")
 
+var JWT_KEY []byte
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	key := os.Getenv("JWT_KEY")
+	if key == "" {
+		log.Fatal("JWT_KEY must be set in .env file")
+	}
+
+	// 2. ISI NILAI di dalam fungsi (Mengisi botol yang sudah ada)
+	// Kita tidak menggunakan 'var' atau ':=' lagi di sini.
+	JWT_KEY = []byte(key)
+}
 type JWTClaims struct {
 	Username string
 	jwt.RegisteredClaims
