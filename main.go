f1093ae370c0b6a3b027308cacc6f1619f202862
thxinsomnia <kompenizeko@gmail.com>
2025-09-11 10:35:16 +0700

    Add CORS and Code Optimalization For Efficiency

--- main.go
+++ main.go
@@ -3,50 +3,51 @@ package main
 import (
 
 	"log"
-	"net/http"
 	"os"
 	"SITEKAD/middlewares"
 	"SITEKAD/controllers/absen"
 	"SITEKAD/controllers/auth"
 	"SITEKAD/controllers/lokasi"
+	"SITEKAD/controllers/profile"
 	"SITEKAD/models"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
-func init() {	
-	models.ConnectDatabase()
-}
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func main() {
+	models.ConnectDatabase()
 	router := gin.Default()
 
-	router.POST("/login", absencontroller.LoginHandler)
-	router.GET("/histori", absencontroller.GetAllAbsen)
-	router.GET("/lokasi", lokasicontroller.GetAllLokasi)
+	//Cors
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:8080"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
+
+
+	router.POST("/login", authcontroller.LoginHandler)
 	router.POST("/aktivasi", authcontroller.Aktivasi)
 	router.GET("/logout", authcontroller.Logout)
-	
 
 	api := router.Group("/api")
 	api.Use(middlewares.AuthMiddleware())
-	api.POST("/absensi", absencontroller.ScanAbsensiHandler)
-	api.GET("/histori", absencontroller.GetAllAbsen)
-	api.GET("/lokasi", lokasicontroller.GetAllLokasi)
+	{
+		api.POST("/absensi", absencontroller.ScanAbsensiHandler)
+		api.GET("/histori", absencontroller.GetAllAbsen)
+		api.GET("/lokasi", lokasicontroller.GetAllLokasi)
+		api.GET("/uhistori", absencontroller.GetHistoryUser)
+		api.GET("/profile", profilecontroller.GetUserProfile)
+	}
 
-	router.ServeHTTP(w, r)
-}
-
-func main() {
-	// Start server
+	// 5. Jalankan Server
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 	log.Printf("Server is running on port %s\n", port)
 
-	// Gunakan handler untuk menangani request HTTP
-	http.HandleFunc("/", Handler) // Memetakan path "/" ke Handler
-
-	// Mulai server
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	router.Run(":" + port)
 }
