dee4eaede0b2c22bcbebc35a43d96e706f1a26a6
thxinsomnia <kompenizeko@gmail.com>
2025-10-24 09:01:04 +0700

    Merge Branch

--- models/lokasipresensi.go
+++ models/lokasipresensi.go
@@ -3,6 +3,7 @@ package models
 type LokasiPresensi struct {
 	
 	ID       int64   `gorm:"primaryKey" json:"id"`
+	CabangId  int64  `gorm:"type bigint" json:"cabang_id"`
 	NamaLokasiPresensi    string `gorm:"type varchar(255)" json:"nama_lokasi_presensi"`
 	LokasiKerjaId int64  `gorm:"type bigint" json:"lokasi_kerja_id"`
 	PenempatanId	int64  `gorm:"type bigint" json:"penempatan_id"`
@@ -13,6 +14,7 @@ type LokasiPresensi struct {
 	Latitude  float64 `gorm:"type float" json:"latitude"`
 	Zona	 string `gorm:"type varchar(255)" json:"zona"`
 	Kodeqr  string `gorm:"type varchar(255)" json:"kodeqr"`
+	Fileqr  string `gorm:"type varchar(255)" json:"fileqr"`
 	IsDeleted int8  `gorm:"type tinyint" json:"is_deleted"`
 	CreatedAt string `gorm:"type:timestamp" json:"created_at"`
 	CreatedBy int64  `gorm:"type bigint" json:"created_by"`
