2714e05ab9abfa7537136d2fb463aeac6e1a21de
thxinsomnia <kompenizeko@gmail.com>
2025-11-14 13:12:30 +0700

    Add new model for new rekap feature, SITEKAD BETA 1.9.1

--- models/laporanabsensi.go
+++ models/laporanabsensi.go
@@ -24,3 +24,15 @@ type LaporanAbsensiHarian struct {
     IsLembur   bool    `json:"is_overtime"`
     JamLembur float64 `json:"overtime_hours"`
 }
+
+type RekapPegawai struct {
+    Nama                string  `json:"nama"`
+    Jabatan             string  `json:"jabatan"`
+    TotalHariKerja      int     `json:"total_hari_kerja"`
+    TotalHadir          int     `json:"total_hadir"`
+    TotalAbsen          int     `json:"total_absen"`
+    HariTelat           int     `json:"hari_telat"`
+    TotalJamKerja       float64 `json:"total_jam_kerja"`
+    RataRataJamKerja    float64 `json:"rata_rata_jam_kerja"`
+    PersentaseKehadiran float64 `json:"persentase_kehadiran"`
+}
